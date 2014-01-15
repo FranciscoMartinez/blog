ad43523ed0657afa92f4e4ca23794a5b3de0ae97
Francisco Martinez <francisco.jm.acevedo@gmail.com>
2014-01-15 15:45:13 -0800

    Added missing 'edit' method to fix update feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,10 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 	
+	def edit
+		@post = Post.find(params[:id])
+	end
+	
 	def update
 		@post = Post.find(params[:id])
 			if @post.update(params[:post])
