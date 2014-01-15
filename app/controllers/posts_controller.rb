f371f6c92c4d24a6d635d3c6e322e7c56ee003b0
Francisco Martinez <francisco.jm.acevedo@gmail.com>
2014-01-15 10:19:25 -0800

    Added Update feature, Links, and Error messages for incomplete forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,28 @@
 class PostsController < ApplicationController
 	def new
+		@post = Post.new
 	end
 	
 	def create
 		@post = Post.new(params[:post])
-		@post.save
-		redirect_to @post
+			if @post.save
+				redirect_to @post
+			else
+				render 'new'
+			end
 	end
 			
 	def show
 		@post = Post.find(params[:id])
+	end
+	
+	def update
+		@post = Post.find(params[:id])
+			if @post.update(params[:post])
+				redirect_to @post
+			else
+				render 'edit'
+			end
 	end
 	
 	def index
@@ -20,8 +33,4 @@ class PostsController < ApplicationController
 		def post_params
 			params.require(:post).permit(:title, :text)
 		end
-	
-
-	
-
 end
